1a40031acfaed95851e45b4c1b8509659def7929
josemiguel736 <robertz@youremail.com>
2024-11-07 12:21:03 +0100

    Renderizado de productos en la app

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,11 @@
+import Product from "../models/Product.js"
 
-export function index(req,res,next){
-        res.render('index',{
-            appName:"NodePop"
-        })
+export async function index(req,res,next){
+        const userID = req.session.userID
+        if(userID){
+            res.locals.products = await Product.find({owner:userID})
+        }
+        console.log(await Product.find({owner:userID}))
+        res.render('index')
 
 }
